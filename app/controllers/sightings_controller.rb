a370a7ee10c6cc8143e543151d1da2a5e450d89a
Grace Shih <vocalise@gmail.com>
2020-01-19 17:32:35 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,10 @@
 class SightingsController < ApplicationController
+  
+    def index
+      @sightings = Sighting.all
+      render json: SightingSerializer.to_serialized_json(@sightings)
+    end
+  
     def show
         @sighting = Sighting.find(params[:id])
         render json: SightingSerializer.to_serialized_json(@sighting)
